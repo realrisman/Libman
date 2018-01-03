82d2f7ce18424a1e2b4fbd005422f75ecf0658d8
Muhamad Risman <real.risman@gmail.com>
2018-01-03 17:02:45 +0700

    Add relation to book and user model in borrow log model

--- app/BorrowLog.php
+++ app/BorrowLog.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class BorrowLog extends Model
 {
-    //
+    public function book()
+    {
+        return $this->belongsTo('App\Book');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
